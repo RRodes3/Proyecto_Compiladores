79ebbd31c16aa69227cf04594a42147c6c101767
RRodes3 <raulrodas939@gmail.com>
2024-01-16 22:05:26 -0600

    Statement Return
    Código para analizar return

--- StmtReturn.java
+++ StmtReturn.java
@@ -1,7 +1,13 @@
+
 public class StmtReturn extends Statement {
     final Expression value;
 
     StmtReturn(Expression value) {
         this.value = value;
     }
+
+    @Override
+    public Object execute(TablaSimbolos tablaSimbolos) {
+        return value.solve(tablaSimbolos);
+     }
 }

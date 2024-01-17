39800f7b8d41caee96cd5f652992a2f70c5950e4
RRodes3 <raulrodas939@gmail.com>
2024-01-16 22:03:42 -0600

    Statement Print
    Código para analizar un print

--- StmtPrint.java
+++ StmtPrint.java
@@ -1,7 +1,18 @@
+
 public class StmtPrint extends Statement {
     final Expression expression;
 
     StmtPrint(Expression expression) {
         this.expression = expression;
+    }
+
+    @Override
+    public Object execute(TablaSimbolos tablaSimbolos) {
+
+        // Resolvemos la expresión
+        Object expresion = expression.solve(tablaSimbolos);
+
+        System.out.println(expresion.toString());
+        return null;
     }
 }
